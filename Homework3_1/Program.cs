2aa2603fc6b8b539820d322c5fdc9f719cb7eb32
Daria Dmitricheva <110775372+12data12@users.noreply.github.com>
2022-09-08 08:46:13 +0300

    Коррекция названия

---
+++ Homework3_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. 
+
+Console.Write("Enter a five-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string digit = Convert.ToString(number);
+
+if(number >= 10000 && number <= 100000)
+{
+    if(digit[0]==digit[4] && digit[1]==digit[3])
+    {
+        Console.WriteLine("Your number is a palindrome.");
+    }
+    else
+    {
+        Console.WriteLine("Your number is not a palindrome.");
+    }
+}
+else
+{
+    Console.WriteLine("The entered number is incorrect. Try again!");
+}
